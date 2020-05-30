45e3488085efa7f34f6063a30b89565ad8ff6ceb
Shekhar Ramola <shekhar.ramola@winds.co.in>
2020-05-30 20:28:56 +0530

    .

--- src/components/intro/intro.js
+++ src/components/intro/intro.js
@@ -6,22 +6,26 @@ export default class Intro extends Component {
         <section
           id="intro"
           style={{
-            background: 'url(img/home.jpg) center center no-repeat',
+            background: 'url(img/cover-img.jpg) center center no-repeat',
             backgroundSize: 'cover'
           }}
           className="intro-section pb-2"
         >
           <div className="container text-center">
             <div data-animate="fadeInDown" className="logo">
-              <img src="img/logo-big.png" alt="logo" width={130} />
+              {/* <img src="img/logo-big.png" alt="logo" width={130} /> */}
+              Hello, hola, नमस्ते !
+              <br/>
+              I am Shekhar Ramola.<br/> I design and build User Interfaces.
+            
             </div>
-
+{/* 
             <h1 data-animate="fadeInDown" className="text-shadow mb-5">
               Hello, hola, नमस्ते !
             </h1>
             <p data-animate="slideInUp" className="h3 text-shadow text-400">
               I am Shekhar Ramola. I design and build User Interfaces.
-            </p>
+            </p> */}
           </div>
         </section>
       </React.Fragment>
